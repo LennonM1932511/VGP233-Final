37780017f93611d5764fe63675f257fe0446fa37
Zijia Li <allenlizijia@outlook.com>
2020-12-11 22:50:22 -0800

    Updated Basic Enemies and Added Boss Enemy

---
+++ Assets/Scripts/Game/EnemyAnimationEvents.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimationEvents : MonoBehaviour
+{
+    [SerializeField]
+    float damage = 10.0f;
+    Player player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    public void DealDamageToPlayer()
+    {
+        player.TakeDamage(damage);
+    }
+
+}
